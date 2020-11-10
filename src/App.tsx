5b61c03ca75a3aa3d53729eaa57518a54abf550d
Marten Bjork <marten@martenbjork.com>
2020-11-10 13:00:42 -0500

    Add ability to update status

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import firebase from "firebase";
+import "firebase/database";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAYuIsZwEi5jYRZG4mh-Ram-1PjQybLzys",
+  authDomain: "meeting-status-d0db6.firebaseapp.com",
+  databaseURL: "https://meeting-status-d0db6.firebaseio.com",
+  projectId: "meeting-status-d0db6",
+  storageBucket: "meeting-status-d0db6.appspot.com",
+  messagingSenderId: "972106182315",
+  appId: "1:972106182315:web:e18f2d708fe672ebb6d1ee",
+};
+
+var app = firebase.initializeApp(firebaseConfig);
+
+var db = firebase.database();
 
 function App() {
+  const [state, setState] = useState("Offline");
+
+  const updateState = (state: string) => {
+    firebase.database().ref("users/1").set({
+      state,
+    });
+    setState(state);
+  };
+
+  const cycle = () => {
+    if (state === "In a meeting") {
+      updateState("Offline");
+    } else {
+      updateState("In a meeting");
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p onClick={cycle}>{state}</p>
     </div>
   );
 }
